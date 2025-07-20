2a0281d9cf15622f54e57afb933874312eb4d512
syarul <syarulazwan.sa@gmail.com>
2025-07-20 16:31:32 +0800

    Theme
    update

--- public/assets/js/custom_full.js
+++ public/assets/js/custom_full.js
@@ -41,3 +41,68 @@ function toggleFullscreenIcons(isFullscreen) {
     closeIcon.classList.add('d-none');
   }
 }
+
+ /* header theme toggle */
+  function toggleTheme() {
+    let html = document.querySelector("html");
+    if (html.getAttribute("data-theme-mode") === "dark") {
+      html.setAttribute("data-theme-mode", "light");
+      html.setAttribute("data-header-styles", "light");
+      html.setAttribute("data-menu-styles", "dark");
+      if (!localStorage.getItem("primaryRGB")) {
+        html.setAttribute("style", "");
+      }
+      html.removeAttribute("data-bg-theme");
+      document.querySelector("#switcher-light-theme").checked = true;
+      document.querySelector("#switcher-menu-dark").checked = true;
+      document
+        .querySelector("html")
+        .style.removeProperty("--body-bg-rgb", localStorage.bodyBgRGB);
+      checkOptions();
+      html.style.removeProperty("--body-bg-rgb2");
+      html.style.removeProperty("--light-rgb");
+      html.style.removeProperty("--form-control-bg");
+      html.style.removeProperty("--input-border");
+      document.querySelector("#switcher-header-light").checked = true;
+      document.querySelector("#switcher-menu-dark").checked = true;
+      document.querySelector("#switcher-light-theme").checked = true;
+      document.querySelector("#switcher-background4").checked = false;
+      document.querySelector("#switcher-background3").checked = false;
+      document.querySelector("#switcher-background2").checked = false;
+      document.querySelector("#switcher-background1").checked = false;
+      document.querySelector("#switcher-background").checked = false;
+      localStorage.removeItem("rixzodarktheme");
+      localStorage.removeItem("rixzoMenu");
+      localStorage.removeItem("rixzoHeader");
+      localStorage.removeItem("bodylightRGB");
+      localStorage.removeItem("bodyBgRGB");
+      html.setAttribute("data-header-styles", "light");
+    } else {
+      html.setAttribute("data-theme-mode", "dark");
+      html.setAttribute("data-header-styles", "dark");
+      html.setAttribute("data-menu-styles", "dark");
+      if (!localStorage.getItem("primaryRGB")) {
+        html.setAttribute("style", "");
+      }
+      document.querySelector("#switcher-dark-theme").checked = true;
+      document.querySelector("#switcher-menu-dark").checked = true;
+      document.querySelector("#switcher-header-dark").checked = true;
+      checkOptions();
+      document.querySelector("#switcher-menu-dark").checked = true;
+      document.querySelector("#switcher-header-dark").checked = true;
+      document.querySelector("#switcher-dark-theme").checked = true;
+      document.querySelector("#switcher-background4").checked = false;
+      document.querySelector("#switcher-background3").checked = false;
+      document.querySelector("#switcher-background2").checked = false;
+      document.querySelector("#switcher-background1").checked = false;
+      document.querySelector("#switcher-background").checked = false;
+      localStorage.setItem("rixzodarktheme", "true");
+      localStorage.setItem("rixzoMenu", "dark");
+      localStorage.setItem("rixzoHeader", "dark");
+      localStorage.removeItem("bodylightRGB");
+      localStorage.removeItem("bodyBgRGB");
+    }
+  }
+  let layoutSetting = document.querySelector(".layout-setting");
+  layoutSetting.addEventListener("click", toggleTheme);
+  /* header theme toggle */
