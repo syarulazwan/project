3fe6ff830786b6b1717ba71e88d2d5b187d54b0e
syarul <syarulazwan.sa@gmail.com>
2025-07-22 17:33:18 +0800

    update
    update

--- app/Http/Controllers/Project/ChatController.php
+++ app/Http/Controllers/Project/ChatController.php
@@ -439,8 +439,6 @@ class ChatController extends Controller
 
     public function ask(Request $request)
     {
-        dd(env('APP_NAME'));
-
         $request->validate([
             'question' => 'required|string|max:500',
             'mode' => 'required|in:general,document',
