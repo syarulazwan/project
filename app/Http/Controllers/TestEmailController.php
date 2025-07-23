e557e2c95510054f19622aa0dd5e3622aa265ed6
syarul <syarulazwan.sa@gmail.com>
2025-07-23 10:01:00 +0800

    update
    update

--- app/Http/Controllers/TestEmailController.php
+++ app/Http/Controllers/TestEmailController.php
@@ -10,15 +10,19 @@ class TestEmailController extends Controller
 {
     public function send()
     {
-        $subject = 'Ujian Emel Laravel';
-        $body = "Hai Syarul Azwan,\n\nIni adalah ujian emel dari sistem Laravel.\n\nTerima kasih.";
 
-        Mail::raw($body, function ($message) use ($subject) {
-            $message->to('syarulazwan.sa@gmail.com')
-                    ->subject($subject);
-        });
+        echo ini_get('disable_functions');
 
-        return 'Emel telah dihantar ke syarulazwan.sa@gmail.com';
+        
+        // $subject = 'Ujian Emel Laravel';
+        // $body = "Hai Syarul Azwan,\n\nIni adalah ujian emel dari sistem Laravel.\n\nTerima kasih.";
+
+        // Mail::raw($body, function ($message) use ($subject) {
+        //     $message->to('syarulazwan.sa@gmail.com')
+        //             ->subject($subject);
+        // });
+
+        // return 'Emel telah dihantar ke syarulazwan.sa@gmail.com';
     }
 
     //  public function send()
