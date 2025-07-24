a27fa93b248b9bd19c0051b6fcc29e5972a63178
syarul <syarulazwan.sa@gmail.com>
2025-07-24 16:05:17 +0800

    update
    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status')->nullable();
             $table->rememberToken();
+            $table->unsignedBigInteger('created_id')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
 
