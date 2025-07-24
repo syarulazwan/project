f2891473a99bbf87605e9fdf774f7456a7014b0c
syarul <syarulazwan.sa@gmail.com>
2025-07-24 17:21:04 +0800

    update
    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -16,7 +16,7 @@
                 </div>
                 <ul class="main-menu">
                     <li class="slide">
-                        <a href="widgets.html" class="side-menu__item">
+                        <a href="widgets.html" class="side-menu__item active">
                             <i class="ri-dashboard-line side-menu__icon"></i>
                             <span class="side-menu__label">Dashboard</span>
                         </a>
@@ -97,6 +97,7 @@
                         </ul>
                     </li>
                 </ul>
+                <div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"> <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path> </svg></div>
             </nav>
         </div>
     </aside>
